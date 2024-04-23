87e59855f8465914481a0a63b2b345268cf67f7d
JoseCondori17 <jose.condori.p@utec.edu.pe>
2024-04-23 09:03:32 -0500

    actualizacion de paquetes

---
+++ frontend/src/lib/types/SignUp.schema.ts
@@ -0,0 +1,17 @@
+import { z } from "zod"
+
+export const SignUpSchema = z.object({
+  firstName: z.string(),
+  lastName: z.string(),
+  email: z.string()
+    .min(8, { message: 'Debe contener al menos 8 caracteres' })
+    .max(50, { message: 'Debe contener maximo 50 caracteres' })
+    .regex(/^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/,'Ingrese un correo valido'),
+  password: z.string()
+    .min(8, { message: 'Debe contener al menos 8 caracteres' })
+    .max(16, { message: 'Debe contener maximo 16 caracteres' })
+    .regex(/\d/,'Debe contener al menos un número')
+    .regex(/[A-Z]/,'Debe contener al menos un letra en mayúscula')
+    .regex(/[a-z]/,'Debe contener al menos un letra en minúscula')
+    .regex(/[!@#$%^&*(),.?":{}|<>]/, 'Debe contener al menos un símbolo especial'),
+})

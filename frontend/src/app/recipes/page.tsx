c248288beafa9bdfbd7187a7f64e1badd4e39c27
JoseCondori17 <83974221+JoseCondori17@users.noreply.github.com>
2024-04-24 21:34:38 +0000

    Avance de card en Recetas

--- frontend/src/app/recipes/page.tsx
+++ frontend/src/app/recipes/page.tsx
@@ -1,25 +1,52 @@
 "use client"
 
-import Image from "next/image"
+import React from 'react'
 import { Icon } from "@iconify/react"
 import {
   Card,
   CardContent,
-  CardDescription,
   CardFooter,
-  CardHeader,
-  CardTitle,
 } from "@/components/ui/card"
 
+
+interface IconProps {
+  name: string
+  active: boolean
+  linear: React.ReactNode
+  bold: React.ReactNode
+}
+
 export default function Recipes() {
+
+  const [icons, setIcons] = React.useState<IconProps[]>([
+    {
+      name: 'like',
+      active: false,
+      linear: (<Icon icon="solar:heart-linear" width={17} height={17}/>),
+      bold: (<Icon icon="solar:heart-bold" className='text-primary' width={17} height={17}/>)
+    },
+    {
+      name: 'save',
+      active: false, 
+      linear: (<Icon icon="solar:bookmark-outline" width={17} height={17}/>),
+      bold: (<Icon icon="solar:bookmark-bold" className='text-primary' width={17} height={17}/>)
+    },
+  ]);
+
+  const handleClick = (index: number) => {
+    const updatedIcons = [...icons];
+    updatedIcons[index].active = !updatedIcons[index].active;
+    setIcons(updatedIcons);
+  };
+
   return (
     <Card className="w-52 h-48">
-      <CardContent>
-        <div className="p-2">
-          <img src="/img/food.jpg" alt="asd" className="rounded-lg w-full"/>
+      <CardContent className="p-2">
+        <div>
+          <img src="/img/food.jpg" alt="asd" className="rounded-lg w-full"/> {/* mejorar la carga de imagen */}
         </div>
       </CardContent>
-      <CardFooter>
+      <CardFooter className="pl-2 pr-2">
         <div className="flex w-full justify-between">
           <div className="flex flex-col">
             <h3 className="text-md font-medium"> Rice Wiee </h3>
@@ -33,8 +60,15 @@ export default function Recipes() {
             </div>
           </div>
           <div className="flex items-center space-x-2 opacity-60">
-            <Icon icon="solar:heart-linear" width={17} height={17}/>
-            <Icon icon="solar:bookmark-outline" width={17} height={17}/>
+            {
+              icons.map((key, idx) => (
+                <button id='idx' onClick={() => handleClick(idx)}>
+                  {
+                    key.active ? key.bold : key.linear
+                  }
+                </button>
+              ))
+            }
           </div>
         </div>
       </CardFooter>

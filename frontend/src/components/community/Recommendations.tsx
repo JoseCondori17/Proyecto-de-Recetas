f169c4d660e7214bf1d62e70eb89ecc963dec17d
CeciliaNatali <cecilia.pilco@utec.edu.pe>
2024-06-05 00:17:12 -0500

    vinculos componente Recomendaciones
    nuevas paginas de ruta para cada categoria en recomendaciones

--- frontend/src/components/community/Recommendations.tsx
+++ frontend/src/components/community/Recommendations.tsx
@@ -4,25 +4,25 @@ const Recommendations = () => {
   return (
     <div className="bg-white rounded-lg p-4 shadow-md">
       <h3 className="text-lg font-bold mb-4">Recomendaciones</h3>
-      <Link href="/home/comunity/healthy" passHref>
+      <Link href="/home/recipes/category/healthy" passHref>
         <div className="mb-2 block cursor-pointer hover:bg-green-400 h-20 flex items-center justify-center rounded-lg bg-green-300">
           <span className="text-2xl mr-4">🥗</span>
           Healthy
         </div>
       </Link>
-      <Link href="/home/comunity/breakfast" passHref>
+      <Link href="/home/recipes/category/breakfast" passHref>
         <div className="mb-2 block cursor-pointer hover:bg-yellow-400 h-20 flex items-center justify-center rounded-lg bg-yellow-300">
           <span className="text-2xl mr-4">🍳</span>
           Breakfast
         </div>
       </Link>
-      <Link href="/home/comunity/sea" passHref>
+      <Link href="/home/recipes/category/sea" passHref>
         <div className="mb-2 block cursor-pointer hover:bg-blue-400 h-20 flex items-center justify-center rounded-lg bg-blue-300">
           <span className="text-2xl mr-4">🐟</span>
           Sea
         </div>
       </Link>
-      <Link href="/home/comunity/fast" passHref>
+      <Link href="/home/recipes/category/fast" passHref>
         <div className="mb-2 block cursor-pointer hover:bg-red-400 h-20 flex items-center justify-center rounded-lg bg-red-300">
           <span className="text-2xl mr-4">🍔</span>
           Fast

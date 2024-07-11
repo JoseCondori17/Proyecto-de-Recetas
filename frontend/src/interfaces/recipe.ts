9938bb91c08102b249d7b66c9eed946ce31f2a8b
JoseCondori17 <jose.condori.p@utec.edu.pe>
2024-07-10 20:38:45 -0500

    interface add

---
+++ frontend/src/interfaces/recipe.ts
@@ -0,0 +1,13 @@
+export interface RecipeInterface {
+  Tipo: string;
+  Imagen: string;
+  Grasas: number;
+  Nombre: string;
+  Instrucciones: string;
+  Calorias: number;
+  Receta_id: number;
+  Proteinas: number;
+  Tiempo_Preparacion: number;
+  Likes: number;
+  Descripcion_ingredientes: string;
+}
